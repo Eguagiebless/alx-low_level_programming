110e2a57c3d095905a07c079e9e0febbba8ecd4c
Eguagieblessing <eguagiebless3@gmail.com>
2023-09-17 18:22:43 -0700

    print numbers from 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,12 @@
 #include "main.h"
 
 /**
-*print_numbers - print 0123456789
-*Return: void
-*/
-
-void print_numbers(void)
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-	char c;
-
-	for (c = '0'; c <= '9'; c++)
-	{
-	_putchar(c);
-	}
-	_putchar('\n');
+    print_numbers();
+    return (0);
 }
