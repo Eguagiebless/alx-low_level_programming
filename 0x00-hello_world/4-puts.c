723840e7edf7d68f8b76b1f557162c066d4dffc6
EguagieBlessing <eguagiebless3@gmail.com>
2025-05-09 05:06:12 -0700

    removed comments inside the main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-int main(void)
 
 /**
- * main- prints a string
- * Return-0 indicate sucess
+ * main - prints a string
+ * Return 0 - sucess
  */
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle")
 	return (0);
