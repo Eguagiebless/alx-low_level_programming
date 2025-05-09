af732121e12adf591b402b809136c2bb73b03c23
EguagieBlessing <eguagiebless3@gmail.com>
2025-05-09 04:55:00 -0700

    hello,world-files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+
+/**
+ * main- prints a string
+ * Return-0 indicate sucess
+ */
+{
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+/**
+ * main-print a string followed by a new line
+ *Return-0 success
+ */
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+	return(0);
+}
