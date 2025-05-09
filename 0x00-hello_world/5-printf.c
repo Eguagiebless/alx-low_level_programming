de6e5aae61f9305923cf965e7f81d55cce257be0
EguagieBlessing <eguagiebless3@gmail.com>
2025-05-09 05:24:17 -0700

    added comments

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-int main(void)
+
 /**
  * main-print a string followed by a new line
- *Return-0 success
+ * Return: 0 succes
  */
+
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art\n");
 	return(0);
