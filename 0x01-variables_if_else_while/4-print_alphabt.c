797263bf5dc0da5287e07088f28bb13d8f9f2275
EguagieBlessing <eguagiebless3@gmail.com>
2025-05-10 07:40:39 -0700

    print all lower case alphabet except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print all lower case alphabet
+ * except q and e
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	char lower_case = 'a';
+
+	while(lower_case <= 'z')
+	{
+		if (lower_case != 'e' && != 'q')
+		{
+			putchar(lower_case);
+			lower_case++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
