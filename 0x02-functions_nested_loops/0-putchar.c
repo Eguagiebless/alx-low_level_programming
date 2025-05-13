2090b19ecc385af1d6a456c6cca9ddb99ad72e8c
EguagieBlessing <eguagiebless3@gmail.com>
2025-05-13 02:54:54 -0700

    print using _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: 0 succss
+ */
+
+int main(void)
+{
+	char print[] = _putchar;
+	int i = 0;
+
+	while (print[i] != '\o')
+	{
+		_putchar(print);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
